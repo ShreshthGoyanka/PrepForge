2c3141f18357deee4795b19763286dfc19444bf6
Shreshth Goyanka <shreshth12goyanka@gmail.com>
2025-04-23 18:02:34 +0530

    Update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
-import { isAuthenticated } from "@/lib/actions/auth.action";
+import { isAuthenticated, getCurrentUser } from "@/lib/actions/auth.action";
 import SignOutButton from "@/components/ui/sign-out";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
@@ -12,15 +12,18 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="root-layout">
-      <nav className="flex items-center justify-between px-4 py-2">
+      <nav className="flex flex-col sm:flex-row items-center justify-between px-4 py-2">
         {/* Logo Section */}
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="PrepForge Logo" width={38} height={32} />
           <h2 className="text-primary-100">PrepForge</h2>
         </Link>
 
-        {/* Sign Out Button */}
-        <div>
+        <div className="flex flex-col sm:flex-row items-center gap-2 text-sm font-semibold text-primary-100 mt-2 sm:mt-0">
+          {isUserAuthenticated && (
+            <h4 className="mr-0 sm:mr-4">Hello, {await getCurrentUser().then(user => user?.name || "Guest")}!</h4>
+          )}
+          {/* Sign Out Button */}
           <SignOutButton />
         </div>
       </nav>
